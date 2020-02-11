adab7b93d371ad8d90d3d9d44340191ac0ad8f65
Kevin Jimenez <kevinja9608@gmail.com>
2020-02-11 13:16:22 -0600

    pre final

--- src/components/event-item/event-item.js
+++ src/components/event-item/event-item.js
@@ -26,13 +26,13 @@ export const EventItem = ({ event }) => {
       <div className='event-item__info'>
         <div className='event-item__header'>
           <h3 className='event-item__name'>{event.name}</h3>
-          <div className='event-item__owner'>
-            <span className='label'>Organizer:</span> {event.owner}
-          </div>
         </div>
         <div className='event-item__date'>{`${event.date.getDate()}-${
           months[event.date.getMonth()]
         }-${event.date.getFullYear()}`}</div>
+        <div className='event-item__owner'>
+          <span className='label'>Organizer:</span> {event.owner}
+        </div>
         <p className='event-item__description'>{event.description}</p>
       </div>
     </article>
